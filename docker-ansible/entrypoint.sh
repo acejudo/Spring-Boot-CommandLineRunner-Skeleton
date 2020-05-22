4bbb5df93eead2e714bf2d0e839696cb6588a58b
Adrian Cejudo <adrian.cejudo@xeridia.com>
2020-05-22 16:31:12 +0200

    added ansible roles

--- docker-ansible/entrypoint.sh
+++ docker-ansible/entrypoint.sh
@@ -2,6 +2,6 @@
 mv /etc/ansible/hosts /etc/ansible/hosts.back
 touch /etc/ansible/hosts
 echo "[localhost]" > /etc/ansible/hosts
-ansible-playbook /usr/local/bin/playbook.yaml
+ansible-playbook /etc/ansible/dev_site.yml
 #Extra line added in the script to run all command line arguments
 exec "$@";
